f0373292da13ff76347563832389a265a873e30e
Viswanath <157232225+ViswanathBalla22@users.noreply.github.com>
2024-02-12 11:17:06 +0000

    INIT

---
+++ venv/main.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pylot as plt 
+import seaborn as sns
+
+%matplotlib inline
+sns.set_style("whitegrid")
+plt.style.use("fivethirtyeight")
+
+simple train = ['call you tonight','call me']
